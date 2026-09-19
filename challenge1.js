59aa323d3186d14d29f9bad63f9c6546ddb82395
sami <samilachgar1@gmail.com>
2026-09-19 09:07:39 +0100

    initial commit

---
+++ challenge1.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let nombre = parseInt(prompt("Entrez un nombre entier :"));
+if ( nombre % 2===0){
+    console.log('le nombre {nombre}est pair.');
+}else {
+    console.log('le nombre {nombre}est impair.');
+}
+let
+lettrel= prompt("Entrez ume lettre :");
