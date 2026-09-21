255d9c64657247a974dc0ab01d49e0e5bd70792a
sami <samilachgar1@gmail.com>
2026-09-21 16:02:29 +0100

    initail commit

--- ch-11-t.js
+++ ch-11-t.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
- const Ville= ["fes","agadir","sale","safi"]
+  const Ville= ["fes","agadir","sale","safi"]
  console.log(Ville.length)
  console.log (Ville)
  Ville[0]="casa"
