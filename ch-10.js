45607e44a219e9150698f13a0a2141479c7dab16
sami <samilachgar1@gmail.com>
2026-09-22 11:02:23 +0100

    initail commit

---
+++ ch-10.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+let personne = { 
+    
+    nom: ("samir"),
+    prenom: "yassin" ,
+    age: 25 ,
+    ville: "safi"
+
+}
+console.log("information de la personne :");
+console.log(`nom :  ${personne.nom}`);
+console.log(`prenom :  ${personne.prenom}`);
+console.log(`age :  ${personne.age}ans`);
+console.log(`ville :  ${personne.ville}`);
