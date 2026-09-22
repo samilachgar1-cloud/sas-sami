36d46523257f088075049055f13b8d5537abb59c
sami <samilachgar1@gmail.com>
2026-09-22 13:08:50 +0100

    initail commit

---
+++ objets.2.js
@@ -0,0 +1,8 @@
+let etudiant = {
+    nom: " sami ",
+    prenom: " lachgar ",
+    notes: [20,2,10,2,0,20,0,32,0,2,]
+}
+console.log(`nom:${etudiant.nom}`);
+console.log(`prenom:${etudiant.prenom}`);
+console.log(`notes:${etudiant.notes}`)
