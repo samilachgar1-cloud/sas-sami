9dae35e4c6bb5ff9952526671e0631758fe483b3
sami <samilachgar1@gmail.com>
2026-09-24 13:27:48 +0100

    initail commit

---
+++ Q-3.js
@@ -0,0 +1,12 @@
+const cars = [
+  { id: 1, brand: "Toyota", model: "Corolla", year: 2020, color: "White", price: 20000, isElectric: false },
+  { id: 2, brand: "Tesla", model: "Model 3", year: 2023, color: "Red", price: 45000, isElectric: true },
+  { id: 3, brand: "Peugeot", model: "208", year: 2019, color: "Blue", price: 15000, isElectric: false },
+  { id: 4, brand: "Volkswagen", model: "Golf", year: 2021, color: "Black", price: 22000, isElectric: false },
+  { id: 5, brand: "Nissan", model: "Leaf", year: 2022, color: "Silver", price: 28000, isElectric: true }
+];
+for ( let car of cars){
+    if ( car.isElectric==true){
+        console.log(car.brand)
+    }
+}
